609e015ce050ac3d6b0f406b5654283384eef79a
valor <valor312@gmail.com>
2020-07-21 20:04:27 +0800

    Done

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,34 @@
-const functions = require('firebase-functions');
+//import firebase functions modules
+const functions = require("firebase-functions");
+//import admin module
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Listens for new messages added to messages/:pushId
+exports.pushNotification = functions.database.ref("/messages/{pushId}").onWrite((change, context) => {
+  console.log("Maziah Push notification triggered");
+
+  console.log(change.after);
+
+  //  Grab the current value of what was written to the Realtime Database.
+  var valueObject = change.after.val();
+
+  console.log(valueObject);
+
+  // Create a notification
+  const payload = {
+    notification: {
+      title: "News",
+      body: valueObject.content,
+      sound: "default",
+    },
+  };
+
+  //Create an options object that contains the time to live for the notification and the priority
+  const options = {
+    priority: "high",
+    timeToLive: 60 * 60 * 24,
+  };
+
+  return admin.messaging().sendToTopic("maziah_topic", payload, options);
+});
